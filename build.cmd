3be2e4ed527071f40171e0a1968dd7892cae97ba
Mārtiņš Možeiko <martins.mozeiko@gmail.com>
2021-11-14 13:47:24 -0800

    use vswhere for Visual Studio location
    This selects newer Visual Studio 2022 when both 2019 and 2022 are installed.

--- build.cmd
+++ build.cmd
@@ -1,9 +1,14 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
 where /Q cl.exe || (
   set __VSCMD_ARG_NO_LOGO=1
-  call "%VS2019INSTALLDIR%\VC\Auxiliary\Build\vcvarsall.bat" amd64 || exit /b 1
+  for /f "tokens=*" %%i in ('"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -requires Microsoft.VisualStudio.Workload.NativeDesktop -property installationPath') do set VS=%%i
+  if "!VS!" equ "" (
+    echo ERROR: Visual Studio installation not found
+    exit /b 1
+  )  
+  call "!VS!\VC\Auxiliary\Build\vcvarsall.bat" amd64 || exit /b 1
 )
 
 if "%VSCMD_ARG_TGT_ARCH%" neq "x64" (
